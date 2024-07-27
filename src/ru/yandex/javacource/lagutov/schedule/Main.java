c79b529ba602a8fd756ca78158be524737008fae
Pavel Lagutov <pashalagutov@yandex.ru>
2024-07-28 00:18:27 +0300

    ТЗ спринт 6

--- src/ru/yandex/javacource/lagutov/schedule/Main.java
+++ src/ru/yandex/javacource/lagutov/schedule/Main.java
@@ -1,14 +1,13 @@
 package ru.yandex.javacource.lagutov.schedule;
-import ru.yandex.javacource.lagutov.schedule.manager.InMemoryTaskManager;
 import ru.yandex.javacource.lagutov.schedule.task.Epic;
 import ru.yandex.javacource.lagutov.schedule.task.Subtask;
 import ru.yandex.javacource.lagutov.schedule.task.Task;
 import ru.yandex.javacource.lagutov.schedule.task.Status;
-import ru.yandex.javacource.lagutov.schedule.manager.TaskManager;
 import ru.yandex.javacource.lagutov.schedule.manager.*;
 
 public class Main {
     public static void main(String[] args) {
+
         HistoryManager manager = Managers.getDefaultHistory();
 
         Task task1 = new Task("задача1","заметка1");
@@ -18,12 +17,12 @@ public class Main {
 
 
 
-        Epic epic1= new Epic("эпик1","1");
+        Epic epic1 = new Epic("эпик1","1");
         Subtask subtask1 = new Subtask("подзадача1","для эпика1",epic1);
         Subtask subtask2 = new Subtask("подзадача2","для эпика1",epic1);
         Subtask subtask3 = new Subtask("подзадача3","для эпика1",epic1);
 
-        Epic epic2= new Epic("эпик2","2");
+        Epic epic2 = new Epic("эпик2","2");
 
         System.out.println("\n------ обращение к задачам :");
         manager.add(subtask1);
