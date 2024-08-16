c20d450f710fcd905d96c68b0d9bdcdf29987af2
Pavel Lagutov <pashalagutov@yandex.ru>
2024-08-17 02:42:43 +0300

    ТЗ спринт 7

--- src/ru/yandex/javacource/lagutov/schedule/manager/ManagerSaveException.java
+++ src/ru/yandex/javacource/lagutov/schedule/manager/ManagerSaveException.java
@@ -1,8 +1,5 @@
 package ru.yandex.javacource.lagutov.schedule.manager;
 
-import java.io.IOException;
-
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String massage) {
         super(massage);
