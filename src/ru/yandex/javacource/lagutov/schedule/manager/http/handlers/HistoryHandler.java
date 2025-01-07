e65e079193196906a5abadf3ec40ae0dd2e6fcf2
Pavel Lagutov <pashalagutov@yandex.ru>
2025-01-07 23:28:03 +0300

    ТЗ спринт 7

--- src/ru/yandex/javacource/lagutov/schedule/manager/http/handlers/HistoryHandler.java
+++ src/ru/yandex/javacource/lagutov/schedule/manager/http/handlers/HistoryHandler.java
@@ -4,20 +4,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import ru.yandex.javacource.lagutov.schedule.manager.Managers;
 import ru.yandex.javacource.lagutov.schedule.manager.TaskManager;
 import ru.yandex.javacource.lagutov.schedule.manager.http.adapters.DurationAdapter;
 import ru.yandex.javacource.lagutov.schedule.manager.http.adapters.LocalDateTimeAdapter;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static ru.yandex.javacource.lagutov.schedule.manager.http.handlers.BaseHttpHandler.*;
 public class HistoryHandler implements HttpHandler {
     private static final Gson gson = new GsonBuilder()
             .serializeNulls()
